8ae418e3ddea448251659fcbd8ab80ac519c7607
Dawid <dawid_fendler98@wp.pl>
2023-12-03 21:41:07 +0100

    Initial commit

---
+++ app/src/main/java/pl/dawidfendler/dailyeaseassistant/MainActivity.kt
@@ -0,0 +1,15 @@
+package pl.dawidfendler.dailyeaseassistant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+        }
+    }
+}
