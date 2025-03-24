ec0599b8007e516bc32818b08f72c9f1d76dcc3c
Dawid <dawid_fendler98@wp.pl>
2025-03-24 19:20:48 +0100

    Task-4.0.0 Configure ktlint for CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
@@ -7,22 +9,37 @@ plugins {
     alias(libs.plugins.firebase) apply false
     alias(libs.plugins.composeP) apply false
     alias(libs.plugins.kover) apply false
+    alias(libs.plugins.ktlint.plugin) apply true
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlinx.kover")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    ktlint {
+        version = "12.2.0"  // Use the updated version here too
+        debug = true
+        android = true
+        outputToConsole = true
+        outputColorName = "RED"
+        ignoreFailures = false
+        reporters {
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.CHECKSTYLE)
+        }
+    }
 
     if (file("build.gradle.kts").exists()) {
         val detektScript = rootProject.file("gradle/detekt.gradle")
-        val ktlintScript = rootProject.file("gradle/ktlint.gradle")
+       // val ktlintScript = rootProject.file("gradle/ktlint.gradle")
 
         if (detektScript.exists()) {
             apply(from = detektScript)
         }
 
-        if (ktlintScript.exists()) {
-            apply(from = ktlintScript)
-        }
+//        if (ktlintScript.exists()) {
+//            apply(from = ktlintScript)
+//        }
     }
 }
 true // Needed to make the Suppress annotation work for the plugins block
